490b937ac608443af4f9474da19ee9352a03fb23
A. Ali <theasgarali@users.noreply.github.com>
2020-02-16 15:37:41 +0000

    Edited the header

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <?php
 
+//HEADER FILE
+
 use OOP\Classes\Auth;
 
 ?>
@@ -8,7 +10,7 @@ use OOP\Classes\Auth;
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Movie Site</title>
+<title>Movies</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/main.css">
@@ -49,6 +51,9 @@ use OOP\Classes\Auth;
 					<?php endif; ?>
 					<li class="nav-item">
 						<a class="nav-link" href="<?php echo BASE_URL;?>/logs.php">Logs</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo BASE_URL;?>/searches.php">Requests</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="<?php echo BASE_URL;?>/logout.php">Logout</a>
