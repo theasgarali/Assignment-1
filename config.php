bc6c544ab3388577a1bd39c25e8476083a2d6702
A. Ali <theasgarali@users.noreply.github.com>
2020-02-16 15:41:36 +0000

    Edited

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-// Configuration details can be amended here.
+// CONFIGURATION details can be amended here.
 
-define('BASE_URL', 'http://192.168.64.2/moviesite/');	// URL of the site where web application is going to run.
-define('DB_HOST', 'localhost');  // Database host Name.
-define('DB_PORT', '3306');  //Port number.
+define('BASE_URL', 'http://192.168.64.2/moviesite/');	// URL of the site where web application is going to run
+define('DB_HOST', 'localhost');  // Database Host Name. 
+define('DB_PORT', '3306');  //Port number
 define('DB_USERNAME', 'root');  // User permission to access the database.
 define('DB_PASSWORD', '');  //leave blank.
-define('DB_DATABASE', 'moviedb');   //database name where all data will be saved and fetched for the site.
+define('DB_DATABASE', 'moviedb');   //database name where all data will be save and fetched for the site.
 
-define('STORAGE_PATH', 'assets'); // Path of the .CSS .JS and images.
+define('STORAGE_PATH', 'assets'); // Path of the CSS, JS, Images
 
-define('ADMIN_EMAIL', 'noreply@moviesite.com'); //admin email added.
+define('ADMIN_EMAIL', 'noreply@moviesite.com'); //admin email added
